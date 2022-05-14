ce3bd27550587c338d5335562e8529ce4dc5ea8b
VBaxter3121 <bax22001@byui.edu>
2022-05-14 19:32:44 +0100

    Minor changes

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -33,12 +33,3 @@ let [modDay, modDate, modMonth, modYear] = [
 ];
 copyright.innerHTML = `<li>&copy; ${year} Southampton Chamber</li><li>Vincent Baxter</li><li>WDD 230 Project</li>`;
 modified.innerHTML = `Last Updated: ${weekday[modDay]}, ${modDate} ${months[modMonth]} ${modYear}`;
-
-
-/*let copyright_text = `Copyright \u00A9 ${new Date().getFullYear()} Vincent Baxter - England`
-let copyright = document.querySelector("#portal-copyright")
-copyright.textContent = copyright_text
-
-let updated_text = `Last updated: ${document.lastModified}`
-let last_updated = document.querySelector("#portal-last-modified")
-last_updated.textContent = updated_text*/
