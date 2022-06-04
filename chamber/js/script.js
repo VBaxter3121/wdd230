0988b36f871ecb7ae1653ab1188767a0f1757c3a
VBaxter3121 <bax22001@byui.edu>
2022-06-05 00:25:07 +0100

    Discover Page

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -37,8 +37,23 @@ let [modDay, modDate, modMonth, modYear, modHour, modMinute, modSecond] = [
 copyright.innerHTML = `<li>&copy; ${year} Southampton Chamber</li><li>Vincent Baxter</li><li>WDD 230 Project</li>`;
 modified.innerHTML = `Last Updated: ${weekday[modDay]}, ${modDate} ${months[modMonth]} ${modYear} ${modHour}:${modMinute}:${modSecond}`;
 
+// Banner
 if (day == 1 || day == 2) {
     const banner = document.querySelector("#banner");
     banner.style.display = "block";
     console.log("Test")
 }
+
+// Days since last visit
+let lastVisit = Number(window.localStorage.getItem("lastVisit"));
+let currentVisit = Date.now();
+const daysParagraph = document.querySelector("#days-since");
+if (lastVisit == "") {
+    daysParagraph.textContent = "N/A"
+} else {
+    let daysSince = currentVisit - lastVisit;
+    daysSince = (daysSince / 86400000).toFixed(0);
+    console.log(`Minutes since last page load: ${daysSince}`);
+    daysParagraph.textContent = daysSince;
+}
+localStorage.setItem("lastVisit", currentVisit);
